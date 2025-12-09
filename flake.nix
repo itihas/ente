1b0f68f0f4a0bccb1fdedead819f647503e70d23
itihas <sahiti93@gmail.com>
2025-12-09 18:55:06 +0530

    package rust-core and rust-wasm (not currently usable by rust-web)

--- flake.nix
+++ flake.nix
@@ -4,13 +4,64 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    crane.url = "github:ipetkov/crane";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; }
     ({ self, moduleWithSystem, ... }: {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, ... }: {
+
+      perSystem = { self', inputs', pkgs, system, lib, ... }: {
         packages = with pkgs; {
+
+          # refer https://github.com/ipetkov/crane/blob/master/examples/quick-start/flake.nix
+          ente-core = let
+            craneLib = inputs.crane.mkLib pkgs;
+            src = craneLib.cleanCargoSource ./rust/core;
+            commonArgs = {
+              inherit src;
+              strictDeps = true;
+              nativeBuildInputs = [ pkg-config ];
+              buildInputs = [ openssl ]
+                ++ lib.optionals pkgs.stdenv.isDarwin [ ];
+            };
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          in craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+
+          # refer https://github.com/ipetkov/crane/blob/master/examples/custom-toolchain/flake.nix
+          ente-wasm = let
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ (import inputs.rust-overlay) ];
+            };
+            craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (p:
+              p.rust-bin.stable.latest.default.override {
+                targets = [ "wasm32-unknown-unknown" ];
+              });
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                (craneLib.fileset.commonCargoSources ./rust/core)
+                ./web/packages/wasm
+              ];
+            };
+          in craneLib.buildPackage {
+            inherit src;
+            # https://github.com/ipetkov/crane/blob/master/docs/faq/workspace-not-at-source-root.md
+            cargoToml = ./web/packages/wasm/Cargo.toml;
+            cargoLock = ./web/packages/wasm/Cargo.lock;
+            postUnpack = ''
+              cd $sourceRoot/web/packages/wasm
+              sourceRoot="."
+            '';
+            cargoExtraArgs = "-p ente-wasm --target wasm32-unknown-unknown";
+            doCheck = false;
+          };
 
           ente-server = buildGoModule {
             pname = "ente-server";
@@ -20,30 +71,38 @@
             buildInputs = [ libsodium ];
             vendorHash = "sha256-napF55nA/9P8l5lddnEHQMjLXWSyTzgblIQCbSZ20MA=";
             doCheck = false;
-            postInstall = "cp -R configurations/ $out/configurations";
+            postInstall = "cp -R ./* $out/";
           };
 
           ente-web = stdenv.mkDerivation (finalAttrs: {
             pname = "ente-web";
             version = "main";
-            src = ./web;
+            src = ./.;
 
-            nativeBuildInputs = [ yarn nodejs yarnConfigHook ];
+            postUnpack = ''
+              cd $sourceRoot/web
+              sourceRoot="."
+            '';
+
+            nativeBuildInputs = [
+              yarn
+              nodejs
+              yarnConfigHook
+              writableTmpDirAsHomeHook
+              self'.packages.ente-wasm
+            ];
+            doCheck = false;
 
             yarnOfflineCache = fetchYarnDeps {
               yarnLock = ./web/yarn.lock;
-              hash = "sha256-omFNobZ+2hb1cEO2Gfn+F3oYy7UDSrtIY4cliQ80CUs=";
+              hash = "sha256-Kr/sOyju+WsdbdS0KN017vtrAsyQoTzn32rltSXykNk=";
             };
-
-            NEXT_PUBLIC_ENTE_ENDPOINT = "ENTE_API_ORIGIN_PLACEHOLDER";
-            NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT = "ENTE_ALBUMS_ORIGIN_PLACEHOLDER";
-            NEXT_PUBLIC_ENTE_PHOTOS_ENDPOINT = "ENTE_PHOTOS_ORIGIN_PLACEHOLDER";
 
             buildPhase = ''
               runHook preBuild
 
               # These commands are executed inside web directory
-              # Build photos. Build output to be served is present at apps/photos/out
+              # Build photos. Build output to be served is present at apps/photos/
               yarn --offline build
 
               # Build accounts. Build output to be served is present at apps/accounts/out
@@ -60,6 +119,8 @@
 
               # Build embed. Build output to be served is present at apps/embed/out
               yarn --offline build:embed
+
+              runHook postBuild
             '';
 
             installPhase = ''
@@ -91,15 +152,17 @@
             nginx = { enable = mkEnableOption "configure"; };
             domain = mkOption { type = types.str; };
             apps = mkOption {
-              type = types.listOf (types.enum [
-                "auth"
-                "cast"
-                "embed"
-                "photos"
-                "share"
-                "accounts"
-              ]);
-              default = [ "auth" "cast" "embed" "photos" "share" "accounts" ];
+              type = types.attrs;
+              default = {
+                accounts = "accounts";
+                auth = "auth";
+                cast = "cast";
+                embed-albums = "embed";
+                photos = "photos";
+                public-albums = "albums";
+                public-locker = "share";
+                family = "family";
+              };
             };
             port = mkOption {
               type = types.int;
@@ -128,7 +191,7 @@
             systemd.services.ente-server = let
               museumConfig = {
                 http.port = cfg.port;
-                apps = genAttrs cfg.apps (n: "${n}.ente.${cfg.domain}");
+                apps = mapAttrs (n: v: "${n}.${cfg.domain}") cfg.apps;
               };
               configDir = pkgs.symlinkJoin {
                 name = "ente-config";
@@ -138,31 +201,45 @@
                     cfg.museumYaml
                   else
                     (pkgs.writeTextDir "museum.yaml" (builtins.toJSON
-                      (mkMerge [ museumConfig cfg.museumExtraConfig ]))))
+                      (recursiveUpdate museumConfig cfg.museumExtraConfig))))
                 ];
               };
             in {
               wantedBy = [ "multi-user.target" ];
               environment = { ENVIROMENT = "production"; };
               serviceConfig = {
-                RootDirectory = configDir;
-                ExecStart = "./bin/museum";
+                User = "ente";
+                Group = "ente";
+                WorkingDirectory = configDir;
+                ExecStart = "${configDir}/bin/museum";
               };
             };
 
-            services.nginx.virtualHosts = {
+            services.nginx.recommendedTlsSettings = true;
+            services.nginx.virtualHosts = let
+              webRoot = pkgs.runCommand "ente-web-configured" { } ''
+                cp -r ${perSystem.config.packages.ente-web} $out
+                find $out -name "*.js" -type f -exec sed -i \
+                  -e 's|NEXT_PUBLIC_ENTE_ENDPOINT|https://${cfg.domain}|g' \
+                  -e 's|NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT|https://albums.${cfg.domain}|g' \
+                  -e 's|NEXT_PUBLIC_ENTE_PHOTOS_ENDPOINT|https://photos.${cfg.domain}|g' \
+                  -e 's|NEXT_PUBLIC_ENTE_SHARE_ENDPOINT|https://share.${cfg.domain}|g' \
+                  {} +
+              '';
+            in {
               ${cfg.domain} = {
                 forceSSL = true;
                 enableACME = true;
                 locations."/".proxyPass =
                   "http://localhost:${toString cfg.port}";
               };
-            } // genAttrs (map (n: "${n}.${cfg.domain}") cfg.apps) (subdomain: {
-              forceSSL = true;
-              enableACME = true;
-              root = "${perSystem.config.packages.ente-web}/${subdomain}";
-              locations."/" = { tryFiles = "$uri $uri.html /index.html"; };
-            });
+            } // mapAttrs' (n: subdomain:
+              (nameValuePair "${subdomain}.${cfg.domain}" {
+                forceSSL = true;
+                enableACME = true;
+                root = "${webRoot}/${subdomain}";
+                locations."/" = { tryFiles = "$uri $uri.html /index.html"; };
+              })) cfg.apps;
           };
         });
     });
